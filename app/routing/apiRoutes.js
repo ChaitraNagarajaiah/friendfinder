e3b58f2e54c1334535054de49b67c09b0cefd9c4
Ravi Mantur <RaviMantur@RavisMac.local>
2019-08-22 21:45:28 -0400

    resized the images:

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,6 +11,7 @@ var friends = require("../data/friends");
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
+        console.log(friends);
     });
 
 
